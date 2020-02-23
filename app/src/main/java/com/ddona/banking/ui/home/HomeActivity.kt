de0a90e6d12a555af5d69da9d6059e5328eb76e6
Trung Doan <doanpt93cntt@gmail.com>
2020-02-23 20:00:16 +0700

    [Implement] add resouce for app model and create layouts

---
+++ app/src/main/java/com/ddona/banking/ui/home/HomeActivity.kt
@@ -0,0 +1,7 @@
+package com.ddona.banking.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+
+}
---
+++ app/src/main/java/com/ddona/banking/ui/transactions/TransactionListActivity.kt
@@ -0,0 +1,7 @@
+package com.ddona.banking.ui.transactions
+
+import androidx.appcompat.app.AppCompatActivity
+
+class TransactionListActivity : AppCompatActivity() {
+
+}
